180121119cf07054cd9324dfb8bfa283584bbf80
Kenneth Trecy Tobias <33564322+KennethTrecy@users.noreply.github.com>
2021-08-08 16:16:35 +0800

    Generalize the token in token info

--- src/token_info.rs
+++ src/token_info.rs
@@ -1,5 +1,3 @@
-use alloc::vec::Vec;
-
 /// Contains the extracted token and its last index occupied in the source.
 /// This token is used as return value for some lexers.
-pub type TokenInfo<'a> = (Token<'a>, usize);
+pub type TokenInfo<T> = (T, usize);
